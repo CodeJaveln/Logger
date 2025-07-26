b45ddc2310b56e613fbc3c7b8757da220b06f215
CodeJaveln <123097758+CodeJaveln@users.noreply.github.com>
2025-07-27 01:37:35 +0200

    fulfilling tests

--- test.c
+++ test.c
@@ -3,9 +3,10 @@
 int main(void) {
     logger_init("test.log");
 
-    logger_info("This is a log test! I have cool variable here: %d", 420);
+    logger_info("This is a log test! I have cool variable here: %s", "wow, I can add extra text!");
     logger_warning("Omg a warning!");
     logger_error("this is not a drill");
+    logger_info("Can I add another info at the bottom?");
 
     logger_close();
 
